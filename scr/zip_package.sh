15ca76897e0ca72712d870b7dc68106ca4389a70
charles <charles.braquet@e.email>
2022-09-15 23:19:25 +0200

    adjust zip package script

--- scr/zip_package.sh
+++ scr/zip_package.sh
@@ -3,26 +3,32 @@
 
 cd $(dirname $0)/..
 
-if [[ ! -f dist/locapoc.cjs ]]; then
-  echo "ERR045: Error, the file dist/locapoc.cjs doesn't exist!"
+if [[ ! -f dist/server/locapoc.cjs ]]; then
+  echo "ERR045: Error, the file dist/server/locapoc.cjs doesn't exist!"
   echo "first run : npm run build"
   exit -1
 fi
 
-TIMESTAMP=$(date +"%Y")
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ZIPNAME="locapoc_v1.2.3.9_${TIMESTAMP}"
 
 ### copy files
-mkdir -p tmp/${ZIPNAME}
-cd tmp
-cp -a ../tests/webui ${ZIPNAME}/
-cp ../dist/locapoc.cjs ${ZIPNAME}/
+mkdir -p dist/${ZIPNAME}
+cd dist
+cp -a ../webui ${ZIPNAME}/
+cp ../dist/server/locapoc.cjs ${ZIPNAME}/
 cp ../scr/linux_runs_locapoc.sh ${ZIPNAME}/
 cp ../scr/windows_runs_locapoc.cmd ${ZIPNAME}/
 
 ### fingerprint
-echo "Fingerprint of ${ZIPNAME}" > ${ZIPNAME}/webui/
-echo "timestamp: ${TIMESTAMP}" >> ${ZIPNAME}/webui/
+cat << EOF > ${ZIPNAME}/webui/locapoc_fingerprint.txt
+> Fingerprint of ${ZIPNAME}
+> timestamp: ${TIMESTAMP}
+> git-remote-url: $(git remote get-url origin)
+> git-branch: $(git rev-parse --abbrev-ref HEAD)
+> git-last-hash: $(git rev-parse HEAD)
+> git-last-commit: $(git log -1)
+EOF
 
 ### packaging
 zip -r ${ZIPNAME}.zip ${ZIPNAME}
