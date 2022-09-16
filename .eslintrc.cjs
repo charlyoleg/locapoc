0490e5109e2a2ff81f49deb3d92b2fe0377e0cd1
charles <charles.braquet@e.email>
2022-09-16 21:16:22 +0200

    add lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+// eslintrc.cjs
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: ["docs/*", "build/*", "dist/*"],
+};
