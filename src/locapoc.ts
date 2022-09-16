689f5173b2d82379e08a7527b33d040dd55ef246
charles <charles.braquet@e.email>
2022-09-16 21:40:56 +0200

    apply prettier

--- src/locapoc.ts
+++ src/locapoc.ts
@@ -1,18 +1,17 @@
 // localpoc.ts
 
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import fs from 'node:fs';
-import process from 'node:process';
-import express from 'express';
-import open from 'open';
-import getport from 'get-port';
-import myrest from './myrest.js';
-
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import fs from "node:fs";
+import process from "node:process";
+import express from "express";
+import open from "open";
+import getport from "get-port";
+import myrest from "./myrest.js";
 
 // sub-routines
-function sleep(ms:number){
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function stop_the_app() {
@@ -20,73 +19,75 @@ async function stop_the_app() {
   process.exit(0);
 }
 
-
 // cli
 const argv = yargs(hideBin(process.argv))
   //.scriptName("locapoc")
-  .version('0.2.0')
+  .version("0.2.0")
   .usage("Usage: $0 --port <port> --directoy <directory-path>")
   .example([
-    ["$0 -p 2022 -d MyPublic", "run the webserver on port 2022 and serve the content of the folder MyPublic"],
-    ])
-  .option('directory', {
-    alias: 'd',
-    type: 'string',
-    description: 'path to the directory to be served.',
-    default: ''
-    })
-  .option('browser', {
-    alias: 'b',
-    type: 'boolean',
-    description: 'Open the browser at the corresponding URL.',
-    default: true
-    })
-  .option('quitable', {
-    alias: 'q',
-    type: 'boolean',
-    description: 'Enable the endpoint /api/quit to stop this http-server.',
-    default: true
-    })
-  .option('cors', {
-    alias: 'c',
-    type: 'boolean',
-    description: 'Set http-header to allow Cross Origin Resource Sharing (CORS).',
-    default: false
-    })
-  .option('port', {
-    alias: 'p',
-    type: 'number',
-    description: 'port-number used by this web-server. If set to 0 an available port-number is automatically selected',
-    default: 0
-    })
-  .option('host', {
-    type: 'string',
-    description: 'Set http-header to allow Cross Origin Resource Sharing (CORS).',
-    default: '127.0.0.1'
-    })
+    [
+      "$0 -p 2022 -d MyPublic",
+      "run the webserver on port 2022 and serve the content of the folder MyPublic",
+    ],
+  ])
+  .option("directory", {
+    alias: "d",
+    type: "string",
+    description: "path to the directory to be served.",
+    default: "",
+  })
+  .option("browser", {
+    alias: "b",
+    type: "boolean",
+    description: "Open the browser at the corresponding URL.",
+    default: true,
+  })
+  .option("quitable", {
+    alias: "q",
+    type: "boolean",
+    description: "Enable the endpoint /api/quit to stop this http-server.",
+    default: true,
+  })
+  .option("cors", {
+    alias: "c",
+    type: "boolean",
+    description:
+      "Set http-header to allow Cross Origin Resource Sharing (CORS).",
+    default: false,
+  })
+  .option("port", {
+    alias: "p",
+    type: "number",
+    description:
+      "port-number used by this web-server. If set to 0 an available port-number is automatically selected",
+    default: 0,
+  })
+  .option("host", {
+    type: "string",
+    description:
+      "Set http-header to allow Cross Origin Resource Sharing (CORS).",
+    default: "127.0.0.1",
+  })
   .strict()
   .parseSync();
 
-
 // sanity checks for argv.directory
-if (argv.directory === '') {
+if (argv.directory === "") {
   console.error(`ERR334: Error, you must specify a not empty directory-path!`);
   process.exit(-1);
 }
-if (! fs.existsSync(argv.directory)) {
+if (!fs.existsSync(argv.directory)) {
   console.error(`ERR339: Error, the path ${argv.directory} doesn't exist!`);
   process.exit(-1);
 }
 
 // useless main function to avoid the top-level await issue by bundling with esbuild
-async function main(){
-
+async function main() {
   // port-number logic
   let portnumber = argv.port;
-  if (portnumber === 0){
+  if (portnumber === 0) {
     portnumber = await getport();
   }
-
 
   // the main
   const app = express();
@@ -99,17 +100,19 @@ async function main(){
       next();
     });
   } else {
-    console.log("INFO328: CORS (cross origin resource sharing) are not permitted.");
+    console.log(
+      "INFO328: CORS (cross origin resource sharing) are not permitted."
+    );
   }
 
   // middleware myrest
-  app.use('/api/myrest', myrest);
+  app.use("/api/myrest", myrest);
 
   // rest-api endpoint /api/quit
   if (argv.quitable) {
-    app.post('/api/quit', (req, res) => {
+    app.post("/api/quit", (req, res) => {
       stop_the_app();
-      return res.send('The http-server will quit in a second');
+      return res.send("The http-server will quit in a second");
     });
   }
 
@@ -118,9 +121,10 @@ async function main(){
 
   // spin the http-server
   app.listen(portnumber, argv.host, () => {
-    console.log(`locapoc serves on port ${portnumber} for host ${argv.host} the directory ${argv.directory} ...`);
+    console.log(
+      `locapoc serves on port ${portnumber} for host ${argv.host} the directory ${argv.directory} ...`
+    );
   });
-
 
   // open the browser
   await sleep(1000);
@@ -131,9 +135,6 @@ async function main(){
   } else {
     console.log(`Please, open the browser at ${url}`);
   }
-
 }
 
 main();
-
-
--- src/myrest.ts
+++ src/myrest.ts
@@ -1,14 +1,14 @@
 // myrest.ts
 // an express middleware with routes
 
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-router.get('/greet', (req, res) => {
+router.get("/greet", (req, res) => {
   res.send(`Hello ${req.query.name} !`);
 });
 
-router.get('/bye', (req, res) => {
+router.get("/bye", (req, res) => {
   res.send(`Bye ${req.query.name} !`);
 });
 
