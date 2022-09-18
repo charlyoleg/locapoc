e059b6e3c8744ad534a8bc239f2325b8c299c396
charles <charles.braquet@e.email>
2022-09-18 21:07:33 +0200

    align swagger definition for json generation

--- src/swaggerDefinition.cjs
+++ src/swaggerDefinition.cjs
@@ -5,9 +5,9 @@ const swaggerJsdoc = require("swagger-jsdoc");
 module.exports = {
   openapi: "3.0.0",
   info: {
-    title: "Helloj Woj",
-    version: "1.2.3",
-    description: "myrest great api",
+    title: "The myrest-API",
+    version: "0.2.0",
+    description: "an example REST-API integrated in locapoc",
   },
   servers: [
     {
