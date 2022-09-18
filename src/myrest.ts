783612367226e71803cfd7adc976e4998180f03f
charles <charles.braquet@e.email>
2022-09-18 02:54:37 +0200

    adding swagger-jsdoc

--- src/myrest.ts
+++ src/myrest.ts
@@ -4,10 +4,28 @@
 import express from "express";
 const router = express.Router();
 
+/**
+ *  @openapi
+ *  /api/myrest/greet:
+ *    get:
+ *      description: generate a synpatic string
+ *      responses:
+ *        200:
+ *          description: Return a json with a msg
+ */
 router.get("/greet", (req, res) => {
   res.json({ msg: `Hello ${req.query.name} !` });
 });
 
+/**
+ *  @openapi
+ *  /api/myrest/bye:
+ *    post:
+ *      description: generate a surprising string
+ *      responses:
+ *        200:
+ *          description: Return a json with a msg
+ */
 router.get("/bye", (req, res) => {
   res.json({ msg: `Bye ${req.query.name} !` });
 });
---
+++ src/swaggerDefinition.cjs
@@ -0,0 +1,13 @@
+// swaggerDefinition.cjs
+
+const swaggerJsdoc = require('swagger-jsdoc');
+
+module.exports = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Helloj Woj',
+    version: '1.2.3',
+    description: 'myrest great api'
+  },
+};
+
