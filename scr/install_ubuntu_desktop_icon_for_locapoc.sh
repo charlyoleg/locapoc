942c2db6ba2db46e37a0bebbea82773f226ebaf5
charles <charles.braquet@e.email>
2022-10-13 16:01:17 +0200

    install script for ubuntu desktop icon

---
+++ scr/install_ubuntu_desktop_icon_for_locapoc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# install_ubuntu_desktop_icon_for_locapoc.sh
+
+SCRIPTDIR=$(dirname $0)
+cd ${SCRIPTDIR}
+
+cat <<SCREOF > locapoc.desktop
+#! /usr/bin/env node
+[Desktop Entry]
+Encoding=UTF-8
+Version=0.1.6
+Name=locapoc
+Comment=Proof-of-Concept for local app
+Exec=${SCRIPTDIR}/linux_runs_locapoc.sh
+Icon=${SCRIPTDIR}/locapoc_icon.svg
+Terminal=true
+Type=Application
+Categories=Development
+SCREOF
+
+
